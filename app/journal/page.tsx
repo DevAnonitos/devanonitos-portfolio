8282d16934398136a863a7dfade7d6f2a8a3a244
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2026-02-19 00:19:27 +0700

    feat: Add initial Journal page component.

---
+++ app/journal/page.tsx
@@ -0,0 +1,11 @@
+import { JournalSection } from "@/components/sections"
+
+const JournalPage = () => {
+  return (
+    <div>
+      <h1>Journal</h1>
+    </div>
+  )
+}
+
+export default JournalPage
