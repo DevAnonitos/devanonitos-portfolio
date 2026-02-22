5855bded51dc8567e74cf9c34474549867306316
Nguyen Duc Gia Bao <91384216+DevAnonitos@users.noreply.github.com>
2026-02-22 14:41:58 +0700

    test: chuẩn hóa cấu hình jest và thêm unit test cơ bản

--- jest.config.ts
+++ jest.config.ts
@@ -2,17 +2,23 @@ import type { Config } from 'jest'
 import nextJest from 'next/jest.js'
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
 })
 
-// Add any custom config to be passed to Jest
 const config: Config = {
+  clearMocks: true,
+  collectCoverageFrom: [
+    '<rootDir>/lib/**/*.{ts,tsx}',
+    '<rootDir>/hooks/**/*.{ts,tsx}',
+    '!**/*.d.ts',
+  ],
   coverageProvider: 'v8',
-  testEnvironment: 'node',
-  // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
 }
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 export default createJestConfig(config)
