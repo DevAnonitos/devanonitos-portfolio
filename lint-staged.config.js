497703920dce1c5dad171aa0bf35954563c506bb
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2026-02-23 16:56:45 +0700

    feat: Configure lint-staged to automate linting, formatting, and type checking for staged files.

---
+++ lint-staged.config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`
+
+module.exports = {
+  // Lint & Format JS/TS files
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand, 'prettier --write'],
+
+  // Type check TS files (Full project check due to tsc limitations with staged files)
+  '**/*.ts?(x)': () => 'tsc --noEmit',
+
+  // Format non-code files
+  '*.{css,scss,json,md,html}': ['prettier --write'],
+}
+
